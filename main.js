3e36a2ad289e3ae9ec0e92b390d7ff9808a7ab20
Hein Htut Zaw <hhz.life.hhz@gmail.com>
2022-01-01 12:10:00 +0630

    opencloseModaljs

--- main.js
+++ main.js
@@ -1,2 +1,51 @@
 function openModal() {
-    document.getElementById("myModal").style.display ="block"};
+    document.getElementById("myModal").style.display ="block"
+};
+
+
+
+function closeModal(){
+    document.getElementById("myModal").style.display="none"
+};
+
+
+var slideIndex=1;
+showSlide(slideIndex);
+
+
+
+function plusSlides(n){
+    showSlide(slideIndex+=n);
+};
+
+
+function currentSlide(n){
+    showSlide(slideIndex=n);
+};
+
+
+
+function showSlide(n){
+
+    
+    var slides=document.getElementsByClassName("mySLides");
+    var demos=document.getElementsByClassName("demos");
+    
+
+    if(n<1){
+        slideIndex=slides.length;
+    }
+
+    if(n>slides.length){
+        slideIndex=1;
+    }
+
+    for ( var i=0; i<slides.length; i++){
+        slides[i].style.display=none;
+    }
+
+    for (var i=0; i<demos.length; i++){
+        demos[i].style.display=none;
+    }
+
+}
