827af5f31650b5c035719d39b40b2b04ee47ab30
Hein Htut Zaw <hhz.life.hhz@gmail.com>
2022-01-03 14:36:51 +0630

    product__amount

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// LIGHT BOX JAVASCRIPT
+
 function openModal() {
     document.getElementById("myModal").style.display ="block"
 };
@@ -51,4 +53,19 @@ function showSlide(n){
     slides[slideIndex-1].style.display="block";
     demos[slideIndex-1].className += " active";
 
+}
+
+
+
+
+
+function plusMinusItem(n){
+    var itemValue= parseInt(document.getElementById("amount").innerText);
+    
+    if(n==1){
+        itemValue++;
+    }else if(n==-1 && itemValue>=1){
+        itemValue--;
+    }
+    document.getElementById("amount").innerText=itemValue;
 }
